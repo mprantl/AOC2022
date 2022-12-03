08423c95fe6fcba307448d0570225792af101913
Hobohias <mprantl90@gmail.com>
2022-12-03 11:59:02 +0100

    Day 3 commit

--- day_three/second/src/main.rs
+++ day_three/second/src/main.rs
@@ -1,13 +1,10 @@
 #![feature(iter_array_chunks)]
 use std::{fs::File, io::Read};
-
 fn main() {
     let mut file = File::open("input.txt").expect("File not found");
     let mut contents = String::new();
     let mut result = 0;
-    file.read_to_string(&mut contents)
-        .expect("Something went wrong reading the file");
-
+    file.read_to_string(&mut contents).expect("Reading failed");
     for [elv_1, elv_2, elv_3] in contents.lines().array_chunks() {
         for i in elv_1.chars() {
             if elv_2.contains(i) && elv_3.contains(i) {
