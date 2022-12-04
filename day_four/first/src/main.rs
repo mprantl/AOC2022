6507b5b272280e6dd3a6cb39503187f97ac6ab82
Hobohias <mprantl90@gmail.com>
2022-12-04 10:42:34 +0100

    Day 4 commit

---
+++ day_four/first/src/main.rs
@@ -0,0 +1,22 @@
+use std::{fs::File, io::Read};
+
+fn main() {
+    let mut file = File::open("input.txt").expect("File not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Something went wrong reading the file");
+
+    let mut result = 0;
+    for line in contents.lines() {
+        let section_id: Vec<u32> = line
+            .split(&[',', '-'])
+            .map(|x| x.parse::<u32>().unwrap())
+            .collect();
+        if (section_id[0] <= section_id[2] && section_id[1] >= section_id[3])
+            || (section_id[2] <= section_id[0] && section_id[3] >= section_id[1])
+        {
+            result += 1;
+        }
+    }
+    println!("Result: {}", result);
+}
