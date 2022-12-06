f142fd3d3782236c2e73b53a281784397f0ff201
Hobohias <mprantl90@gmail.com>
2022-12-06 12:24:01 +0100

    Day 6 commit

---
+++ day_six/first_and_second/src/main.rs
@@ -0,0 +1,26 @@
+use std::{fs::File, io::Read};
+fn main() {
+    let contents = read_input();
+    find_unique_slice(contents.clone(), 4);
+    find_unique_slice(contents.clone(), 14);
+}
+fn find_unique_slice(contents: Vec<char>, window_size: usize) {
+    let iter = contents.windows(window_size);
+    for i in iter.enumerate() {
+        let (window, index) = (i.1, i.0);
+        if !(1..window.len()).any(|i| window[i..].contains(&window[i - 1])) {
+            println!(
+                "Window: {:?} index: {index} offset({window_size}): {}",
+                window,
+                index + window_size
+            );
+            break;
+        }
+    }
+}
+fn read_input() -> Vec<char> {
+    let mut file = File::open("input.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents.chars().collect()
+}
